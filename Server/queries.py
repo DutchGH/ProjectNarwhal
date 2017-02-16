36eb4846bacccaba851bdfe490acda42c6673c6c
Luke Roberts <sc15lar@leeds.ac.uk>
2017-02-16 13:32:03 +0000

    Added more queries.

--- Server/queries.py
+++ Server/queries.py
@@ -20,6 +20,47 @@ def admins():
 def rooms():
     return models.Rooms.query.all()
 
-#Adds an item to a list
-def addToList(list,item):
-    list
+#Adds an item to a list, works to add either classes to a delegates classList or
+#to add delegates to a classes waiting list.
+def addToList( list,item ):
+    #If adding to a waiting list
+    if(list.__tablename__ == 'Class'):
+        list.waitList.append( item )
+    #If adding to a class list
+    else:
+        list.classList.append( item )
+    db.session.commit()
+
+#Checks for unique username. Returns false if the username is taken.
+def checkUserName( name ):
+    dels = delegates()
+    for i in dels:
+        if( i.username == name ):
+            return False
+    return True
+
+#Returns the delegates taking a class
+def classAttendence( thisClass ):
+    dels=delegates()
+    at = []
+    for i in dels:
+        for g in i.classList:
+            if g.classID == thisClass.classID:
+                at.append( g )
+    return at
+
+#Returns the number of delegates taking a class
+def classAttendenceLen( thisClass ):
+    return len( classAttendence( thisClass ) )
+
+#Checks if a class is at capacity
+def capacity( thisClass ):
+    if( thisClass.capacity >= ( classAttendenceLen( thisClass ) ) ):
+        return False
+    else:
+        return True
+
+#Removes an item from the database
+def remove(item):
+    db.session.delete(item)
+    db.session.commit()
