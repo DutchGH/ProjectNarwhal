6ef9e8e00f3cf2741fe1e63a3fff02b5dd099d60
Luke Roberts <Luke Roberts>
2017-05-01 19:12:55 +0100

    Fixed onClick problem.

--- Server/app/static/js/jsFunctions.js
+++ Server/app/static/js/jsFunctions.js
@@ -3,9 +3,6 @@ function changeCards(item){
   document.getElementById("tutorBox").innerHTML = item[1];
   document.getElementById("test5").innerHTML = item[2];
   document.getElementById("buildingBox").innerHTML = item[3];
-<<<<<<< HEAD
   document.getElementById("cardImage").src = item[4];
-=======
-  document.getElementById("cancelClass").href = "/cancel";
->>>>>>> 62e7edbb49f954938065041d104255d48d26422a
+  document.getElementById("cancelClass").onclick = function(){alert(item[5])};
 }
